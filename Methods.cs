3c3233bbfbf72cf559c2dfa31f1d5c11cfcee683
PallaviKatari <pallavikatari@gmail.com>
2023-07-25 10:18:13 +0530

    Add project files.

---
+++ Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch35
+{
+    internal class Methods
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Methods Class");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//namespace - collection of classes
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch35
+{
+    /// <summary>
+    /// Basics of C#
+    /// internal - access specifier - default access specifier for the class - within the current assembly
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Class.Method
+            Console.WriteLine("Welcome to C#");
+            Console.ReadLine();
+        }
+    }
+}
