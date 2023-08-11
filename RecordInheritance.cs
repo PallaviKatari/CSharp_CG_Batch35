deaed9e48d2df7634f38ca5d1fad66f7c9edf58b
PallaviKatari <pallavikatari@gmail.com>
2023-08-11 12:47:17 +0530

    commit_11.08.2023

---
+++ RecordInheritance.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch35
+{
+    record Person1
+    {
+        public string pname;
+        public int age;
+        public Person1()
+        {
+            pname = "abc";
+            age = 20;
+        }
+        public virtual void show()
+        {
+            Console.WriteLine("Show() method of Person record!");
+            Console.WriteLine($"Name: {pname} Age: {age}");
+        }
+    }
+    record Employee1 : Person1
+    {
+        public string organization;
+        public Employee1()
+        {
+            organization = "XYZ Ltd.";
+        }
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("Show() method of Employee record!");
+            Console.WriteLine($"Organization: {organization}");
+        }
+    }
+    record Student1 : Person1
+    {
+        public string course;
+        public Student1()
+        {
+            course = "MBA";
+        }
+        public override void show()
+        {
+            base.show();
+            Console.WriteLine("Show() method of Student record!");
+            Console.WriteLine($"Course: {course}");
+        }
+    }
+    class RecordInheritance
+    {
+        static void Main(string[] args)
+        {
+            Person1 r;
+            r = new Employee1();
+            r.show();
+
+            r = new Person1();
+            r.show();
+
+            r = new Student1();
+            r.show();
+
+            Console.WriteLine("\n");
+            var v = new Employee1();
+            v.show();
+            Console.WriteLine("\n");
+            var v1 = new Student1();
+            v1.show();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RecordStruct.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch35
+{
+    record Item
+    {
+        public int ItemNo { get; set; }
+        public string ItemName { get; set; }
+        public double price { get; set; }
+    }
+    class RecordStruct
+    {
+        static void GetDiscount(Item c)
+        {
+            double discount = 0.12;
+            double p = c.price;
+            var r = c with { price = p - p * discount };
+            //Inside the function
+            Console.WriteLine("Record Inside the function...");
+            Console.WriteLine($"Item No.: {r.ItemNo}  Item Name: {r.ItemName} Item Price: {r.price}");
+        }
+        static void Main(string[] args)
+        {
+            Item ob = new Item();
+            ob.ItemNo = 11;
+            ob.ItemName = "Fan";
+            ob.price = 1600;
+
+            //Before Discount
+            Console.WriteLine("Record before calling the function...");
+            Console.WriteLine($"Item No.: {ob.ItemNo}  Item Name: {ob.ItemName} Item Price: {ob.price}");
+            GetDiscount(ob);
+            //After Discount
+            //The change is not reflected in the Main() method since the records are passed by value.
+            Console.WriteLine("Record after calling the function...");
+            Console.WriteLine($"Item No.: {ob.ItemNo}  Item Name: {ob.ItemName} Item Price: {ob.price}");
+            Console.ReadLine();
+        }
+    }
+}
